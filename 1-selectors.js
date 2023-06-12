ed662ca592d8b2955e4dacff615911176c746c80
emrahugurlu2003 <emrahugurlu2023@gmail.com>
2023-06-12 17:27:25 +0300

    closest Eklenince

--- 1-selectors.js
+++ 1-selectors.js
@@ -22,7 +22,7 @@ const itemListArray = Array.from(classDegiskeni);
 //array içinde dolaşmak için forEach kullanılır.
 //Her bir item için ne yapacağını da fonlsiyon olarak yazdım.
 //hem log etti hem de her birinde attribute set derek ilerledi!
-itemListArray.forEach((item) => console.log((item.style.color = `purple`)));
+//itemListArray.forEach((item) => console.log((item.style.color = `purple`)));
 
 //HTML collection doner bunu array haline çevirmek için 2.YOL
 const itemListSpread = [...itemListArray];
@@ -30,7 +30,7 @@ const itemListSpread = [...itemListArray];
 //console.log(itemListSpread);
 
 //Şimdi bu array ile oynayalım. orjinali bozmadan manuple edelim
-itemListSpread.map((item) => console.log((item.style.color = `red`)));
+//itemListSpread.map((item) => console.log((item.style.color = `red`)));
 
 const tagSecme = document.querySelector(`h1`);
 const classSecme = document.querySelector(`.list`);
@@ -42,12 +42,18 @@ const listeSecme = document.querySelectorAll(`ul li`);
 //Yukarı yonde traverse: child->parent
 const listYukari = document.querySelector(`li`);
 //ilk li yi buldu, yani HTML yi
-console.log(
+/* console.log(
   listYukari.parentElement.parentElement.parentElement.parentElement
     .parentElement
-);
+); */
 //once ul'ye oradan da section'a oradan da maine'e oradan da body'e oradan da html'ye gitti
 //aynısını paretNode ile de yapmak mumkun.
+
+//uzun uzun ilermek yerine hızlıca atmaka da mumkun.
+//En yakın section'a zıplayalım:
+
+const listYukari2 = document.querySelector(`li`); /* en başa donelim */
+console.log(listYukari2.closest(`section`));
 
 //Aşağı yonde traverse: parent->child
 
